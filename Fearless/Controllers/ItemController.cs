77cc24095069b5aea05abf8ea864e4c049fc6f2b
DamonFord <xdamonfordx@gmail.com>
2021-03-09 11:33:07 -0500

    first commit

---
+++ Fearless/Controllers/ItemController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using Fearless.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Fearless.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemController : ControllerBase
+    {
+        private static readonly List<Person> _People = new List<Person>();
+
+        // GET: api/<ItemController>
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok(_People);
+        }
+
+        // GET api/<ItemController>/5
+        [HttpGet("{id}")]
+        public ActionResult GetItem(int id)
+        {
+            var person = _People.FirstOrDefault(x => x.ID == id);
+            if (person == null)
+                return NotFound(id);
+            
+            return Ok(person);
+        }
+
+        [HttpPut("{name}")]
+        public ActionResult AddItem(string name)
+        {
+            //add new item to the list
+            var maxId = 1;
+            if (_People.Count() > 0)
+                maxId = _People.Max(x => x.ID) + 1;
+
+            var newItem = new Person()
+            {
+                ID = maxId,
+                Name = name
+            };
+            _People.Add(newItem);
+
+            return Ok(newItem.ID);
+        }
+
+        // DELETE api/<ItemController>/5
+        [HttpDelete("{id}")]
+        public ActionResult DeleteItem(int id)
+        {
+            var person = _People.FirstOrDefault(x => x.ID == id);
+            if (person == null)
+                return NotFound(id);
+
+            _People.Remove(person);
+            return Ok();
+        }
+    }
+}
